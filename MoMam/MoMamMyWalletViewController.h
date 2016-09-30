9925ca561f641fb93fcdae71631eea8914ecb136
송준일 <wnsdlf114@naver.com>
2016-09-30 22:11:07 +0900

    MyWallet 뷰컨트롤러 파일 생성

---
+++ MoMam/MoMamMyWalletViewController.h
@@ -0,0 +1,13 @@
+//
+//  MoMamMyWalletViewController.h
+//  MoMam
+//
+//  Created by 송준일 on 2016. 9. 30..
+//  Copyright © 2016년 Song Jun Il. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoMamMyWalletViewController : UIViewController
+
+@end
